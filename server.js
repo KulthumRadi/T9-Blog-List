64e7f64bfba47f1911162c9978efedca15d9d1fe
KulthumRadi <kulthumradi94@gmail.com>
2022-06-06 22:36:38 +0300

    creating the form to add blog and to edit it part 3 and continue the server

--- server.js
+++ server.js
@@ -1,9 +1,23 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const Article = require('./models/article')
+const articleRouter = require('./routes/articles')
+const methodOverride = require('method-override')
 const app = express()
 
-app.set("view engine", "ejs")
-app.arguments('/', (req, res )=>{
-
-    res.send("hello")
+mongoose.connect('mongodb://localhost/blog', {
+  useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true
 })
+
+app.set('view engine', 'ejs')
+app.use(express.urlencoded({ extended: false }))
+app.use(methodOverride('_method'))
+
+app.get('/', async (req, res) => {
+  const articles = await Article.find().sort({ createdAt: 'desc' })
+  res.render('articles/index', { articles: articles })
+})
+
+app.use('/articles', articleRouter)
+
 app.listen(5000)
